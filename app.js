980808fe53647e997350da3599e371f0c069644e
luthf-ai <110040741+luthf-ai@users.noreply.github.com>
2022-10-13 20:40:20 +0700

    added banner and slider

--- app.js
+++ app.js
@@ -4,4 +4,13 @@ let menuBtn = document.querySelector('#menu-btn');
 menuBtn.addEventListener('click', () => {
     navbar.classList.toggle('active');
     menuBtn.classList.toggle('fa-times');
-    });
+});
+
+var counter = 1;
+setInterval(function(){
+    document.getElementById('radio' + counter).checked = true;
+    counter++;
+    if(counter > 3){
+        counter = 1;
+    }
+}, 5000);
